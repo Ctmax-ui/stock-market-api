a1280a54aa655cc96757b147cfcc5c45bbb973cd
Ctmax-ui <debjeetbiswas121@gmail.com>
2024-08-28 11:08:07 +0530

    fixed timing issue

--- routes/appRoutes.js
+++ routes/appRoutes.js
@@ -28,43 +28,43 @@ router.route('/data').get((req,res)=>{
 })
 
 
-// router.route('/open/:sotckName').get(async (req, res) => {
-//     // console.log(req.params.sotckName);
-//     try {
-//         const data = await fetchData('https://www.nseindia.com/api/option-chain-indices?symbol=NIFTY');
-//         res.json(data);
-//     } catch (error) {
-//         // console.error('Fetch error:', error);
-//         res.status(404).send('stock not found');
-//     }
-// });
+router.route('/open/:sotckName').get(async (req, res) => {
+    // console.log(req.params.sotckName);
+    try {
+        const data = await fetchData('https://www.nseindia.com/api/option-chain-indices?symbol=NIFTY');
+        res.json(data);
+    } catch (error) {
+        // console.error('Fetch error:', error);
+        res.status(404).send('stock not found');
+    }
+});
 
-// router.route('/stocks/:sotckName').get(async (req, res) => {
-//     // console.log(req.params.sotckName);
+router.route('/stocks/:sotckName').get(async (req, res) => {
+    // console.log(req.params.sotckName);
 
-//     let dynamicParam = 'NIFTY-50'
+    let dynamicParam = 'NIFTY-50'
 
-//     try {
-//         const data = await fetchData(`https://www.nseindia.com/api/chart-databyindex-dynamic?index=${dynamicParam.replace(/-/g, '%20')}&indices=true`);
-//         res.json(data);
-//     } catch (error) {
-//         // console.error('Fetch error:', error);
-//         res.status(404).send('stock not found');
-//     }
-// });
+    try {
+        const data = await fetchData(`https://www.nseindia.com/api/chart-databyindex-dynamic?index=${dynamicParam.replace(/-/g, '%20')}&indices=true`);
+        res.json(data);
+    } catch (error) {
+        // console.error('Fetch error:', error);
+        res.status(404).send('stock not found');
+    }
+});
 
 
-// router.route('/eqstocks').get(async (req, res) => {
-//     // console.log(req.params.sotckName);
+router.route('/eqstocks').get(async (req, res) => {
+    // console.log(req.params.sotckName);
 
-//     try {
-//         const data = await fetchData(`https://www.nseindia.com/api/equity-stock?index=allcontracts`);
-//         res.json(data);
-//     } catch (error) {
-//         // console.error('Fetch error:', error);
-//         res.status(404).send('stock not found');
-//     }
-// });
+    try {
+        const data = await fetchData(`https://www.nseindia.com/api/equity-stock?index=allcontracts`);
+        res.json(data);
+    } catch (error) {
+        // console.error('Fetch error:', error);
+        res.status(404).send('stock not found');
+    }
+});
 
 
 router.route('/calculate/sum').get(async (req, res) => {
